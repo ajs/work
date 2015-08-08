c9ff811f317fa29e41cf1baaa339a42e66058fba
Aaron Sherman <ajs@everquote.com>
2015-08-08 00:10:10 -0400

    Many improvements made to existing apporaches and some new approaches

--- rand7by5.py
+++ rand7by5.py
@@ -1,17 +1,34 @@
 # A truly random number between 0 and 4
 
 import os
+import time
 import random
 import hashlib
 import unittest
 import numpy
+
 
 # Several approaches to the problem: given f() returns a truly random
 # number in the range 0-4, write a function that returns a uniformly
 # random number in the range 0-6.
 
 
-def _rand_n(target, bits=16):
+def _map_range(value, source_range, target_range):
+    """
+    Given a source range like 2**32, map a value from that
+    range to a value in target range, e.g. 5. For best
+    results (to minimize the impact of target_range not being
+    a divisor of source_range, make sure that source_range is
+    several orders of magnitude larger than target_range).
+    """
+
+    granularity = source_range // target_range
+    if value > (granularity * target_range):
+        return value % target_range
+    else:
+        return value // granularity
+
+def _rand_n(target, bits=32):
     """
     Return an integer in the range 0 - `target`, non-inclusive,
     by extracting `bits` number of random bits from the system's
@@ -27,8 +44,7 @@ def _rand_n(target, bits=16):
         raise ValueError("Funny, wiseguy")
 
     n = os.urandom(bits//8)
-    f = float(int(n.encode('hex'), 16))
-    return int((f / (2**bits)) * target)
+    return _map_range(int(n.encode('hex'), 16), 2**bits, target)
 
 def _rand5():
     """Our known random source of integers in range 0-4"""
@@ -62,6 +78,8 @@ def _populate_bits2(func, source_bits, bits=32):
     Different strategy for some result as _populate_bits.
     """
 
+    bits += 2
+
     shifts = bits - source_bits + 1
     if shifts <= 0:
         raise ValueError(
@@ -69,21 +87,7 @@ def _populate_bits2(func, source_bits, bits=32):
     t = 0
     for i in range(0, shifts):
         t ^= func() << i
-    return t
-
-def _hash_as_int(s, hashfunc=hashlib.md5, hashbits=128):
-    """
-    Perform the given hash on string `s` and return its
-    integer representation.
-    """
-
-    hashed = hashfunc(s).digest()
-    hexed = hashed.encode("hex")
-    t = 0
-    for i in range(1, (hashbits//4)+1):
-        hex_bits = hexed[-i]
-        t += int(hex_bits, 16) << ((i-1)*4)
-    return t
+    return ( (t >> 1) & (2**(bits-2)-1) )
 
 def rand7by5_prng(prng=random.Random):
     """
@@ -104,7 +108,7 @@ def rand7by5_prng2(prng=random.Random):
     _prng = prng(seed)
     return _prng.randint(0, 6)
 
-def rand7by5_hash():
+def rand7by5_hash(hashfunc=hashlib.md5, hashbits=128):
     """
     Given a hash function, build a string from our source, hash it
     and then normalize the hash as an integer to our range via
@@ -118,11 +122,13 @@ def rand7by5_hash():
     will be uniform.
     """
 
-    seed = _populate_bits(_rand5, source_bits=3)
-    bigfricken_n = _hash_as_int(str(seed))
-    range_n = 2**128
-    rnum = (bigfricken_n * 7) // range_n
-    return rnum
+    seed = _populate_bits2(_rand5, source_bits=3)
+    hashed = hashfunc(str(seed)).digest()
+    hexed = hashed.encode("hex")
+    # Strip off high and low bit, which hashing functions might set
+    # to 1 due to the ways hashes get used.
+    stripped = (int(hexed, 16) >> 1) & (2**(hashbits-2)-1)
+    return _map_range(stripped, 2**(hashbits-2), 7)
 
 def rand7by5_lottery():
     """
@@ -143,16 +149,12 @@ def rand7by5_lottery():
             if s[i] >= win:
                 yield i
 
-    def _lottery(win, size=None, mapping=None):
-        if size and mapping:
-            assert size == len(mapping)
-        elif size:
-            mapping = range(size)
-        elif mapping:
-            size = len(mapping)
-        scores = [ 0 for _ in range(size) ]
+    def _lottery(win, mapping=None):
+        size = len(mapping)
+        ordering = range(size)
+        scores = [ 0 for _ in ordering ]
         while True:
-            for i in range(size):
+            for i in ordering:
                 scores[i] += _rand5()
             winners = list(_winners(scores, win))
             count = len(winners)
@@ -162,14 +164,49 @@ def rand7by5_lottery():
                 # Resolve ties recursively
                 return mapping[_lottery(win=win, mapping=winners)]
 
-    return _lottery(win=10, size=7)
+    return _lottery(win=20, mapping=range(7))
 
 def rand7by5_modmap():
+    """
+    This is the simplest solution, but for very large target ranges,
+    it will take quite a long time...
+
+    Update:
+
+    This is actually wrong... not by a lot, but it's wrong. The
+    absolute distribution of all possible results is:
+
+        0: 11172
+        1: 11177
+        2: 11172
+        3: 11158
+        4: 11144
+        5: 11144
+        6: 11158
+
+    """
+
     t = 0
     for i in range(7):
         t += _rand5()
         t %= 7
     return t
+
+def rand7by5_timing(bits=32, timing_checks=100):
+    """Use timing on _rand5 to build an entropy pool"""
+
+    # XXX We need to comb out the signal, here per RFC 4086
+    t = 0
+    for i in range(timing_checks):
+        bit = i % bits
+        start = time.time()
+        _ = _rand5()
+        end = time.time()
+        delta = end - start
+        assert delta != 0
+        low = int(delta * 1000000) & 3 # low 2 bits of microseconds
+        t ^= (low << bit) & (2**bits - 1)
+    return _map_range(t, 2**bits, 7)
 
 
 class RandTest(unittest.TestCase):
@@ -188,7 +225,7 @@ class RandTest(unittest.TestCase):
             self.assertLess(n, size)
             seen[n] += 1
 
-        self.assertNotEqual(min(seen), 0)
+        self.assertNotEqual(min(seen), 0, "Distribution is bad: %r" % (seen,))
 
         if size == 7:
             dev = numpy.std(seen)
@@ -197,11 +234,14 @@ class RandTest(unittest.TestCase):
                 baseline_seen[_rand7()] += 1
             baseline_dev = numpy.std(baseline_seen)
 
-            if baseline_dev < dev:
-                self.assertTrue(
-                    baseline_dev >= (dev/2),
-                    "baseline stddev: %s; observed stddev: %s" % (
-                        baseline_dev, dev))
+            dev_ratio = abs(baseline_dev - dev)/baseline_dev
+
+            #print "Differing by %.3f (%.3f base %.3f observed)" % (
+            #    dev_ratio, baseline_dev, dev)
+
+            self.assertTrue(
+                dev < baseline_dev or dev_ratio < 0.5,
+                "stddev: baseline %s; observed %s" % (baseline_dev, dev))
 
         return seen
 
@@ -212,6 +252,13 @@ class RandTest(unittest.TestCase):
             stars = "".join(["*" for _ in range(int((hist[i]/m) * 61))])
             print "%-4d: %-60s" % (i, stars)
 
+    def test_map_range(self):
+        """Make sure we map ranges correctly"""
+
+        self.assertEqual(_map_range(9, 15, 5), 3)
+        self.assertEqual(_map_range(0, 2**32, 5), 0)
+        self.assertEqual(_map_range(300, 2**32, 5), 0)
+
     def test_rand_n_10(self):
         """Coverage of output range for _rand_n(10)"""
 
@@ -219,23 +266,6 @@ class RandTest(unittest.TestCase):
 
     def test_rand5(self):
         self._random_coverage(_rand5, 5)
-
-    def test_hash_as_int(self):
-        def _makefake(x):
-            class _fakehash(object):
-                def __init__(self, s):
-                    pass
-
-                def digest(self):
-                    return x
-            return _fakehash
-
-        n = _hash_as_int("", _makefake('\xff'), 8)
-        self.assertEqual(n, 255)
-        n = _hash_as_int("", _makefake('\x00\x00'), 8)
-        self.assertEqual(n, 0)
-        n = _hash_as_int("", _makefake('\x00\xff'), 8)
-        self.assertEqual(n, 255)
 
     def test_rand7by5_prng(self):
         hist = self._random_coverage(rand7by5_prng, 7)
@@ -256,6 +286,10 @@ class RandTest(unittest.TestCase):
     def test_rand7by5_modmap(self):
         hist = self._random_coverage(rand7by5_modmap, 7)
         self._hist_coverage("rand7by5_modmap", hist)
+
+    def test_rand7by5_timing(self):
+        hist = self._random_coverage(rand7by5_timing, 7)
+        self._hist_coverage("rand7by5_timing", hist)
 
 
 if __name__ == '__main__':
